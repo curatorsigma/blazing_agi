a17180bbc648a6500df4f37f83cc561151c76be3
Jonathan Schleucher <jschleucher@jgdresden.de>
2024-08-27 15:51:17 +0200

    remove unnecessary import

--- blazing_agi_macros/src/lib.rs
+++ blazing_agi_macros/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, Expr, ExprTuple, Ident, ItemFn, PatTuple};
+use syn::{parse_macro_input, Expr, ExprTuple, Ident, ItemFn};
 
 /// Given an async fn, create an AGIHandler from it
 ///
